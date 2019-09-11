5e74f650c725a97c87924ee5c4daf248bd0bf923
Khaninder Kommagoni <khaninder.kommagoni@gmail.com>
2019-09-10 20:55:27 -0400

    Update

--- Class2/Class2/Program.cs
+++ Class2/Class2/Program.cs
@@ -6,54 +6,24 @@ namespace Class2
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("How many years experience do you have ...");
+            Console.WriteLine("How many yrs exp do you have");
             try
             {
-                string input = Console.ReadLine();
-                int exp = int.Parse(input);
-                if (exp == 0)
+                int exp = int.Parse(Console.ReadLine());
+                int i = 0;
+                for (i = 0; i < exp; i++)
                 {
-                    Console.WriteLine("Need lots of practice");
-                    Console.ReadLine();
-                    Console.WriteLine("Press any key to exit");
-                    Console.ReadKey(true);
+                    Console.WriteLine("You have " + exp.ToString() + " Of Experience");
                 }
-                else if (exp == 1)
-                {
-                    Console.WriteLine("Looks LIke you have a little experience");
-                    Console.ReadLine();
-                    Console.WriteLine("Press any key to exit");
-                    Console.ReadKey(true);
-                }
-                else if (exp == 2)
-                {
-                    Console.WriteLine("Wow! You've been doing this for a little while");
-                    Console.ReadLine();
-                    Console.WriteLine("Press any key to exit");
-                    Console.ReadKey(true);
-                }
-                else if (exp > 2)
-                {
-                    Console.WriteLine("You are an expert");
-                    Console.ReadLine();
-                    Console.WriteLine("Press any key to exit");
-                    Console.ReadKey(true);
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Input Given");
-                    Console.ReadLine();
-                    Console.WriteLine("Press any key to exit");
-                    Console.ReadKey(true);
-                }
+                Console.ReadLine();
             }
             catch
             {
-                Console.WriteLine("Invalid Input Given");
-                Console.WriteLine("Press any key to exit");
-                Console.ReadKey(true);
+
+                Console.WriteLine("Invalid Input");
+                Console.ReadLine();
             }
         }
+
     }
 }
